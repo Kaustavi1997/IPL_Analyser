08ff98cc9990b1a69e717036aeaea70d2da06687
“Kaustavi <kaustavisen8@gmail.com>
2020-07-09 19:46:25 +0530

    Added:UC1_testcases for top batting average

--- src/test/java/com/bridgelabz/IplAnalyserTest.java
+++ src/test/java/com/bridgelabz/IplAnalyserTest.java
@@ -1,4 +1,47 @@
 package com.bridgelabz;
 
+import com.bridgelabz.exception.IplAnalyserException;
+import com.bridgelabz.model.IPLMostRunCSV;
+import com.bridgelabz.service.IplAnalyser;
+import com.google.gson.Gson;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.List;
+
 public class IplAnalyserTest {
+    private static final String IPL_CSV_FILE_PATH = "./src/test/resources/MostRun.csv";
+
+    @Test
+    public void givenIndianCensusCSVFileReturnsCorrectRecords() {
+        try {
+            IplAnalyser iplAnalyser = new IplAnalyser();
+            int numOfRecords = iplAnalyser.loadIplRunData(IPL_CSV_FILE_PATH);
+            Assert.assertEquals(101, numOfRecords);
+        } catch (IplAnalyserException e) {
+            e.printStackTrace();
+        }
+    }
+    @Test
+    public void givenIplData_ShouldReturnTop1BattingAverage() {
+        try {
+            IplAnalyser iplAnalyser = new IplAnalyser();
+            iplAnalyser.loadIplRunData(IPL_CSV_FILE_PATH);
+            List sortedData = iplAnalyser.getTopBattingAverage();
+            Assert.assertEquals(83.2, sortedData.get(0));
+        } catch (IplAnalyserException e) {
+            e.printStackTrace();
+        }
+    }
+    @Test
+    public void givenIplData_ShouldReturnTop2BattingAverage() {
+        try {
+            IplAnalyser iplAnalyser = new IplAnalyser();
+            iplAnalyser.loadIplRunData(IPL_CSV_FILE_PATH);
+            List sortedData = iplAnalyser.getTopBattingAverage();
+            Assert.assertEquals(69.2, sortedData.get(1));
+        } catch (IplAnalyserException e) {
+            e.printStackTrace();
+        }
+    }
 }
